985552644475edbf3b8418411b9fc0073bfd7b00
Priyanshu Gupta <priyanshug.cs.25@nitj.ac.in>
2026-08-23 01:40:41 +0530

    Update useGetItemsByCity.jsx

--- frontend/src/hooks/useGetItemsByCity.jsx
+++ frontend/src/hooks/useGetItemsByCity.jsx
@@ -1,25 +1,41 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setItemsInMyCity, setShopsInMyCity, setUserData } from '../redux/userSlice'
+import axios from "axios"
+import React, { useEffect } from "react"
+import { serverUrl } from "../App"
+import { useDispatch, useSelector } from "react-redux"
+import { setItemsInMyCity } from "../redux/userSlice"
 
 function useGetItemsByCity() {
-    const dispatch=useDispatch()
-    const {currentCity}=useSelector(state=>state.user)
-  useEffect(()=>{
-  const fetchItems=async () => {
-    try {
-           const result=await axios.get(`${serverUrl}/api/item/get-by-city/${currentCity}`,{withCredentials:true})
-            dispatch(setItemsInMyCity(result.data))
-           console.log(result.data)
-    } catch (error) {
-        console.log(error)
-    }
-}
-fetchItems()
- 
-  },[currentCity])
+    const dispatch = useDispatch()
+    const { currentCity } = useSelector(state => state.user)
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            try {
+                const city = currentCity?.trim() || "NoCity"
+
+                const result = await axios.get(
+                    `${serverUrl}/api/item/get-by-city/${encodeURIComponent(city)}`,
+                    {
+                        withCredentials: true
+                    }
+                )
+
+                dispatch(setItemsInMyCity(result.data))
+
+                console.log("Items:", result.data)
+
+            } catch (error) {
+                console.log(
+                    "FETCH ITEMS ERROR:",
+                    error.response?.data || error.message
+                )
+            }
+        }
+
+        fetchItems()
+    }, [currentCity])
+
+    return null
 }
 
 export default useGetItemsByCity
